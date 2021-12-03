b877bae2639f245ec3d2e0a2dcf6bdc32c3125de
Víctor Pérez Sanmartín <764914@unizar.es>
2021-12-03 13:01:36 +0100

    Primary goal

--- src/test/kotlin/websockets/ElizaServerTest.kt
+++ src/test/kotlin/websockets/ElizaServerTest.kt
@@ -37,7 +37,7 @@ class ElizaServerTest {
         assertEquals("The doctor is in.", list[0])
     }
 
-    @Disabled
+    
     @Test
     fun onChat() {
         val latch = CountDownLatch(4)
@@ -46,8 +46,8 @@ class ElizaServerTest {
         val client = ElizaOnOpenMessageHandlerToComplete(list, latch)
         container.connectToServer(client, URI("ws://localhost:$port/eliza"))
         latch.await()
-        // assertEquals(XXX, list.size) COMPLETE ME
-        // assertEquals(XXX, list[XXX]) COMPLETE ME
+        assertEquals(4, list.size)
+        assertEquals("Do you really think so?", list[3])
     }
 
 }
@@ -68,8 +68,8 @@ class ElizaOnOpenMessageHandlerToComplete(private val list: MutableList<String>,
     fun onMessage(message: String, session: Session)  {
         list.add(message)
         latch.countDown()
-        // if (COMPLETE ME) {
-        //    COMPLETE ME
-        // }
+        if (latch.count==1L) {
+            session.basicRemote.sendText("I think i got it")
+        }
     }
 }
